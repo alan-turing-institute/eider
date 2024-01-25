28e9343e088bbd86c30a78ecf0c8e0e60e853fed
Helen Duncan <hduncan@turing.ac.uk>
2024-01-25 10:43:51 +0000

    minisparra first commit

---
+++ minisparra/R/helper_functions.R
@@ -0,0 +1,13 @@
+# Adjust date formats to DD-MM-YYYY
+adjust_date_format <- function(dx) {
+  for (i in 1:dim(dx)[2]) {
+    if (class(dx[1, i]) == "Date") {
+      dx[, i] <- format(dx[, i], "%d-%m-%Y")
+    }
+  }
+  dx
+}
+
+print_hello <- function(){
+  print("Hello world")
+}
