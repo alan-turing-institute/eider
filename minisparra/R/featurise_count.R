3df1db92569d69218af443f0acff6f25f6646ccd
Jonathan Yong <yongrenjie@gmail.com>
2024-02-14 15:05:44 +0000

    slight code simplification

--- minisparra/R/featurise_count.R
+++ minisparra/R/featurise_count.R
@@ -4,10 +4,11 @@
 #' @param all_tables List of all input tables (passed in from read_all_tables).
 #' @param source_table_file Filename of the source table to read from.
 #' @param filter_obj A filter object to apply to the source table.
-#' @param id_column_name Name of the patient ID column in the source table.
-#' @param missing_value The value to use for patients who have no matching rows
-#' in the source table.
 #' @param output_column_name Name of the output column.
+#' @param id_column_name Name of the patient ID column in the source table.
+#' Defaults to 'id'.
+#' @param missing_value The value to use for patients who have no matching rows
+#' in the source table. Defaults to 0.
 #'
 #' @return A list with the following elements:
 #' - feature_table: A data frame with one row per patient ID and one column
@@ -22,16 +23,16 @@
 featurise_count <- function(all_tables,
                             source_table_file,
                             filter_obj,
+                            output_column_name,
                             id_column_name = "id",
-                            missing_value = 0,
-                            output_column_name) {
+                            missing_value = 0) {
   source_table <- all_tables[[source_table_file]]
 
   feature_table <- source_table %>%
     filter_all(filter_obj) %>%
     magrittr::extract2("passed") %>%
-    group_by(.data[[id_column_name]]) %>%
     rename(id = !!id_column_name) %>%
+    group_by(id) %>%
     summarise(!!output_column_name := n())
 
   list(
--- minisparra/R/featurise_sum.R
+++ minisparra/R/featurise_sum.R
@@ -6,11 +6,11 @@
 #' @param filter_obj A filter object to apply to the source table.
 #' @param column_to_sum_name Name of the column which provides the values to be
 #' summed.
+#' @param output_column_name Name of the output column.
 #' @param id_column_name Name of the patient ID column in the source table.
 #' Defaults to 'id'.
 #' @param missing_value The value to use for patients who have no matching rows
 #' in the source table. Defaults to 0.
-#' @param output_column_name Name of the output column.
 #'
 #' @return A list with the following elements:
 #' - feature_table: A data frame with one row per patient ID and one column
@@ -26,16 +26,16 @@ featurise_sum <- function(all_tables,
                           source_table_file,
                           filter_obj,
                           column_to_sum_name,
+                          output_column_name,
                           id_column_name = "id",
-                          missing_value = 0,
-                          output_column_name) {
+                          missing_value = 0) {
   source_table <- all_tables[[source_table_file]]
 
   feature_table <- source_table %>%
     filter_all(filter_obj) %>%
     magrittr::extract2("passed") %>%
-    group_by(.data[[id_column_name]]) %>%
     rename(id = !!id_column_name) %>%
+    group_by(id) %>%
     summarise(!!output_column_name := sum(.data[[column_to_sum_name]]))
 
   list(
