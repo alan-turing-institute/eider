3c4eaa7af7c4d054b61e35f2381b22119ad66b48
Jonathan Yong <yongrenjie@gmail.com>
2024-02-21 16:27:00 +0000

    Add overall transform() pipeline

---
+++ minisparra/tests/testthat/test-transform.R
@@ -0,0 +1,42 @@
+test_that("transform", {
+  ae2_table_name <- "../data/ae2.csv"
+
+  all_table_filenames <- c(ae2_table_name)
+  all_feature_json_filenames <- c(
+    "../spec/test_join1.json",
+    "../spec/test_join2.json"
+  )
+  joined_feature_table <- transform(
+    all_table_filenames,
+    all_feature_json_filenames
+  )
+
+  # Check the result
+  orig_table <- read.csv(ae2_table_name)
+  ids <- data.frame(id = sort(unique(orig_table$id)))
+  diag_101_expected <- orig_table %>%
+    filter(diagnosis_1 == 101 | diagnosis_2 == 101 | diagnosis_3 == 101) %>%
+    group_by(id) %>%
+    summarise(diag_101_count = n()) %>%
+    select(c(id, diag_101_count))
+  diag_102_expected <- orig_table %>%
+    filter(diagnosis_1 == 102 | diagnosis_2 == 102 | diagnosis_3 == 102) %>%
+    group_by(id) %>%
+    summarise(diag_102_count = n()) %>%
+    select(c(id, diag_102_count))
+  feature_table_expected <- ids %>%
+    left_join(diag_101_expected, by = "id") %>%
+    mutate(diag_101_count = tidyr::replace_na(diag_101_count, 0)) %>%
+    left_join(diag_102_expected, by = "id") %>%
+    mutate(diag_102_count = tidyr::replace_na(diag_102_count, 0))
+
+  expect_equal(joined_feature_table$id, feature_table_expected$id)
+  expect_equal(
+    joined_feature_table$diag_101_count,
+    feature_table_expected$diag_101_count
+  )
+  expect_equal(
+    joined_feature_table$diag_102_count,
+    feature_table_expected$diag_102_count
+  )
+})
