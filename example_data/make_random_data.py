8c946c1591c315a7ec847dfb8d712b25be5779db
Helen Duncan <hduncan@turing.ac.uk>
2024-02-28 01:18:33 +0000

    improve random A&E data

--- example_data/make_random_data.py
+++ example_data/make_random_data.py
@@ -19,9 +19,42 @@ def make_random_num_prescribed_items(nrows):
     return rng.poisson(2, nrows) + 1
 
 def make_random_diagnois_choice(ranges, with_NA, nrows):
-    if with_NA:
-        ranges.append("NA")
-    return list(rng.choice(ranges, size=nrows))
+    diag_choice = list(rng.choice(ranges, size=nrows))
+    if with_NA: # replace ~5% of the values with NA
+        n_replace = int(nrows/20)+1
+        for _ in range(n_replace):
+            idx = rng.choice(range(len(diag_choice)))
+            diag_choice[idx] = "NA"
+    return diag_choice
+
+def make_three_random_diagnosis_choices(ranges, nrows, n=3):
+    diag_choice = ranges.copy()
+    d1 = make_random_diagnois_choice(ranges=diag_choice, with_NA=False, nrows=nrows)
+    d2 = make_random_diagnois_choice(ranges=diag_choice, with_NA=True, nrows=nrows)
+    d3 = make_random_diagnois_choice(ranges=diag_choice, with_NA=True, nrows=nrows)
+
+    # Ensure all d1 and d2 are different
+    for i, (a, b) in enumerate(zip(d1, d2)):
+        if a == b:
+            d2[i] = a + rng.choice([-1,1])
+
+    # Ensure d3[i] is NA if d2[i] is NA
+    for i, x in enumerate(d2):
+        if x=="NA":
+            d3[i] = "NA"
+
+    # Ensure no duplicates across d1, d2, d3 except NA
+    for i, x in enumerate(d3):
+        if x != "NA" and (d1[i] == x or d2[i]==x):
+            tmp = ranges.copy()
+            vals_to_remove = list(set([d1[i], d2[i]]))
+            for v in vals_to_remove:
+                if v in tmp:
+                    tmp.remove(v)
+            d3[i] = rng.choice(tmp)
+    return d1, d2, d3
+
+
 
 def make_random_pis_data(max_ID, nrows, start_date, end_date, min_sec, max_sec):
     pis_data = pd.DataFrame({
@@ -33,13 +66,16 @@ def make_random_pis_data(max_ID, nrows, start_date, end_date, min_sec, max_sec):
     pis_data.to_csv("./random_pis_data.csv", index=False)
 
 def make_random_ae_data(max_ID, nrows, start_date, end_date, diagnosis_choice):
+
+    d1, d2, d3 = make_three_random_diagnosis_choices(ranges=diagnosis_choice, nrows=nrows)
+
     ae_data = pd.DataFrame({
     'id': make_random_IDs(max_ID=max_ID, nrows=nrows), 
     'time': make_random_dates(start=start_date, end=end_date, nrows=nrows), 
     'attendance_category': [1]*nrows,
-    'diagnosis_1': make_random_diagnois_choice(ranges=diagnosis_choice, with_NA=False, nrows=nrows),
-    'diagnosis_2': make_random_diagnois_choice(ranges=diagnosis_choice, with_NA=True, nrows=nrows),
-    'diagnosis_3': make_random_diagnois_choice(ranges=diagnosis_choice, with_NA=True, nrows=nrows),
+    'diagnosis_1': d1,
+    'diagnosis_2': d2,
+    'diagnosis_3': d3,
     }, index=range(nrows))
     ae_data.to_csv("./random_ae_data.csv", index=False)
 
