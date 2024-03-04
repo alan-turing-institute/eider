1637aa98f40c3b75f898739f4d3527f122d7c306
Helen Duncan <hduncan@turing.ac.uk>
2024-03-04 11:41:42 +0000

    add example files to more permenant directory

---
+++ minisparra/R/example.R
@@ -0,0 +1,18 @@
+#' Return path to the example datasets provided with the package
+#'
+#' @param file Name of the file to be used from inst/extdata. If NULL all files will be listed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' minisparra_example() Returns a list of all the random patient data
+#' minisparra_example("random_ae_data.csv") Returns path to the random A&E type file
+minisparra_example <- function(file = NULL) {
+  current_package_name <- "miniSPARRA01"
+  if (is.null(file)) {
+    dir(system.file("extdata", package = current_package_name))
+  } else {
+    system.file("extdata", file, package = current_package_name, mustWork = TRUE)
+  }
+}
