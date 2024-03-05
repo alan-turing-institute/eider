e3d9ad826f1c815d867015201a2f8a1b703fcf50
Jonathan Yong <yongrenjie@gmail.com>
2024-03-05 18:54:30 +0000

    Start writing a vignette

--- minisparra/R/transform.R
+++ minisparra/R/transform.R
@@ -2,18 +2,18 @@
 #'
 #' @param data_sources A list mapping unique table identifiers to either
 #' the file path from which they should be read from, or the data frame itself.
-#' @param all_feature_json_filenames A vector of file paths to the feature JSON
+#' @param feature_filenames A vector of file paths to the feature JSON
 #' specifications.
 #'
 #' @return A data frame with the feature tables joined together
 #' @export
-transform <- function(data_sources, all_feature_json_filenames) {
+transform <- function(data_sources, feature_filenames) {
   # Read all the tables
   all_tables <- read_data(data_sources)
 
   # Read in each feature JSON file and calculate each individual feature
   features <- lapply(
-    all_feature_json_filenames,
+    feature_filenames,
     function(json_fname) featurise(all_tables, json_fname)
   )
 
