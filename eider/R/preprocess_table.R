0231ca3db199e2d2d34fe14956d470291fe1d78f
Helen Duncan <hduncan@turing.ac.uk>
2024-03-19 17:39:28 +0000

    expand doc example

--- eider/R/preprocess_table.R
+++ eider/R/preprocess_table.R
@@ -10,7 +10,7 @@ preprocess_table <- function(input_table,
   log_debug("Mutating original data table")
 
   # identify useful variables
-  grouping_columns <- spec$grouping_columns
+  grouping_column <- spec$grouping_column
   preprocess_on <- spec$preprocess$on
   retain_min <- spec$preprocess$retain_min
   retain_max <- spec$preprocess$retain_max
@@ -20,24 +20,24 @@ preprocess_table <- function(input_table,
   input_table[[retain_max]] <- as.Date(input_table[[retain_max]])
 
   # Find the earliest (admission) and latest (discharge) date for each group
-  min_dates <- input_table %>% group_by(across(all_of(grouping_columns)),
+  min_dates <- input_table %>% group_by(across(all_of(grouping_column)),
                                         across(all_of(preprocess_on))) %>%
     summarise(min_date = min(!!sym(retain_min)))
-  max_dates <- input_table %>% group_by(across(all_of(grouping_columns)),
+  max_dates <- input_table %>% group_by(across(all_of(grouping_column)),
                                         across(all_of(preprocess_on))) %>%
     summarise(max_date = max(!!sym(retain_max)))
 
   # Replace the minimum (admission likely) in the original dataframe
   # with the earliest date
   input_table <- input_table %>%
-    left_join(min_dates, by = c(grouping_columns, preprocess_on)) %>%
+    left_join(min_dates, by = c(grouping_column, preprocess_on)) %>%
     mutate(!!retain_min := min_date) %>%
     select(-min_date)
 
   # Replace the maximum (discharge likely) in the original dataframe
   # with the latest date
   input_table <- input_table %>%
-    left_join(max_dates, by = c(grouping_columns, preprocess_on)) %>%
+    left_join(max_dates, by = c(grouping_column, preprocess_on)) %>%
     mutate(!!retain_max := max_date) %>%
     select(-max_date)
 
