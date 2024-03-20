c16953d4e906f14a64afacd054cd2b67a2014329
Helen Duncan <hduncan@turing.ac.uk>
2024-03-20 16:28:43 +0000

    test and nicer names

--- eider/R/featurise.R
+++ eider/R/featurise.R
@@ -14,6 +14,7 @@
 #' - missing_value: The value to use for patients who have no matching rows in
 #'                  the source table. This value is passed downstream to the
 #'                  function which joins all the feature tables together.
+#' @export
 featurise <- function(all_tables,
                       spec,
                       context = NULL) {
